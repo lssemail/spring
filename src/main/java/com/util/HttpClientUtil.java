c697c8c9602796c427349b0dfda0d2af43062563
lssemail <lssemail@126.com>
2017-12-04 22:25:16 +0800

    httpclient util

---
+++ src/main/java/com/util/HttpClientUtil.java
@@ -0,0 +1,136 @@
+package com.util;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HttpClientUtil {
+
+
+    public static String doGet(String url, JSONObject params){
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String resultString = "";
+        CloseableHttpResponse response = null;
+        try {
+            URIBuilder builder = new URIBuilder(url);
+            if(params != null){
+                for(Map.Entry<String, Object> entry : params.entrySet()){
+                    builder.addParameter(entry.getKey(), entry.getValue().toString());
+                }
+            }
+            URI uri = builder.build();
+
+            HttpGet httpGet = new HttpGet(uri);
+            response = httpClient.execute(httpGet);
+            if(response.getStatusLine().getStatusCode() == 200){
+                resultString = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(response != null){
+                    response.close();
+                }
+                httpClient.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        return resultString;
+    }
+
+    private String doGet(String url){
+
+        return doGet(url, null);
+    }
+
+
+    public static String doPost(String url, JSONObject params){
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String resultString = "";
+        CloseableHttpResponse response = null;
+        try {
+
+            HttpPost httpPost = new HttpPost(url);
+            if(params != null){
+                List<NameValuePair> paramList = new ArrayList<>();
+                for(Map.Entry<String, Object> entry : params.entrySet()){
+                    paramList.add(new BasicNameValuePair(entry.getKey(), entry.getValue().toString()));
+                }
+                //模拟表单
+                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramList, "utf-8");
+                httpPost.setEntity(entity);
+            }
+
+            response = httpClient.execute(httpPost);
+            resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                response.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        return resultString;
+
+    }
+
+    public static String doPost(String url, String json){
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String resultString = "";
+        CloseableHttpResponse response = null;
+        try {
+
+            HttpPost httpPost = new HttpPost(url);
+            StringEntity stringEntity = new StringEntity(json, ContentType.APPLICATION_JSON);
+            httpPost.setEntity(stringEntity);
+
+            response = httpClient.execute(httpPost);
+            resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                response.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+        return resultString;
+
+    }
+
+    public static void main(String[] args) {
+
+        String result = doGet("http://www.baidu.com", null);
+        System.out.print(result);
+    }
+}
